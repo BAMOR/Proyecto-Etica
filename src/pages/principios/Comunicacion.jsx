5fde669fbc2b935eb082171b859c09c3cf8f6428
paulo <paulojosueb58@gmail.com>
2025-10-02 19:12:44 -0600

    primeros cambios

---
+++ src/pages/principios/Comunicacion.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export const Comunicacion = () => {
+
+
+    const [practica, setPractica] = useState ({
+        
+
+    })
+    return (
+        <>
+
+        
+
+
+
+        </>
+    )
+}
+
+export default Comunicacion;
